a81ee3842f4f15d8ff740a9aa0c49f2a65209eb3
Marcin Kostrzewa <marckostrzewa@gmail.com>
2023-05-08 16:09:52 +0200

    compile for rust

---
+++ src/starkware/stark/committed_trace_binding.cc
@@ -0,0 +1,27 @@
+//
+// Created by Marcin Kostrzewa on 08/05/2023.
+//
+#include "starkware/stark/committed_trace.h"
+#include "starkware/algebra/fields/extension_field_element.h"
+#include "starkware/channel/prover_channel.h"
+#include "starkware/channel/verifier_channel.h"
+#include "starkware/commitment_scheme/merkle/merkle_commitment_scheme.h"
+#include "starkware/commitment_scheme/table_verifier.h"
+#include "starkware/commitment_scheme/table_verifier_impl.h"
+#include "starkware/error_handling/test_utils.h"
+#include "starkware/stark/test_utils.h"
+#include "starkware/stark/utils.h"
+#include "starkware/utils/task_manager.h"
+#include <iostream>
+
+extern "C" {
+void *doTheDooblyDoop(size_t trace_length, size_t blowup) {
+    std::cout << "doTheDooblyDoop(" << trace_length << ", " << blowup << ")" << std::endl;
+    const starkware::EvaluationDomain evaluation_domain(trace_length, blowup);
+    starkware::ProverChannel prover_channel;
+    starkware::TableProverFactory<starkware::BaseFieldElement> table_prover_factory =
+            starkware::GetTableProverFactory<starkware::BaseFieldElement>(&prover_channel);
+    return new starkware::CommittedTraceProver<starkware::BaseFieldElement>(UseOwned(&evaluation_domain), 1, table_prover_factory);
+}
+
+}

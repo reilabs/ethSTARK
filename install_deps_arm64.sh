34ff25bfa15cf5858c5182c90ae58cec666461ab
Grzegorz Swirski <grzegorz@swirski.name>
2023-06-06 16:17:55 +0200

    relative path to ios toolchain

--- install_deps_arm64.sh
+++ install_deps_arm64.sh
@@ -14,7 +14,7 @@ mkdir -p build
     rm -rf ./build/googletest
     git clone -b release-1.8.0 https://github.com/google/googletest.git build/googletest
     cd build/googletest
-    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=/Users/gswirski/Code/wc/ethSTARK/ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor
+    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor
     make -j
     make install
 )
@@ -23,7 +23,7 @@ mkdir -p build
     rm -rf ./build/gflags
     git clone -b v2.2.1 https://github.com/gflags/gflags.git build/gflags
     cd build/gflags
-    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=/Users/gswirski/Code/wc/ethSTARK/ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor
+    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor
     make -j
     make install
 )
@@ -32,7 +32,7 @@ mkdir -p build
     rm -rf ./build/glog
     git clone -b 0.3.5-ios https://github.com/reilabs/glog.git build/glog
     cd build/glog
-    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=/Users/gswirski/Code/wc/ethSTARK/ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor
+    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor
     make -j
     make install
 )
@@ -41,7 +41,7 @@ mkdir -p build
     rm -rf ./build/benchmark
     git clone -b v1.4.0 https://github.com/google/benchmark.git build/benchmark
     cd build/benchmark
-    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=/Users/gswirski/Code/wc/ethSTARK/ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor # -DCMAKE_BUILD_TYPE=Release 
+    cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64 -DCMAKE_INSTALL_PREFIX=../../vendor # -DCMAKE_BUILD_TYPE=Release 
     make -j
     make install
 )

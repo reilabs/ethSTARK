3e39e932b11a44f6b833fc7cd6085012d238a198
Grzegorz Swirski <grzegorz@swirski.name>
2023-06-14 19:36:46 +0200

    table_prover_factory_factory

--- src/starkware/stark/committed_trace_binding.cc
+++ src/starkware/stark/committed_trace_binding.cc
@@ -13,10 +13,6 @@ using namespace starkware;
 
 extern "C" {
 
-void free_obj(void *input) {
-    delete input;
-}
-
 void *new_input(size_t trace_length) {
     size_t n_columns = 1;
     Prng prng;
@@ -29,46 +25,37 @@ void *new_input(size_t trace_length) {
     return trace_columns;
 }
 
-void *new_prover(size_t trace_length, size_t blowup) {
+void free_obj(void *input) {
+    delete input;
+}
+
+void *new_prover_factory() {
+    Prng prng;
+    ProverChannel *prover_channel = new ProverChannel(ProverChannel(prng.Clone()));
+
+    TableProverFactory<BaseFieldElement> table_prover_factory =
+        GetTableProverFactory<BaseFieldElement>(prover_channel);
+
+    return new TableProverFactory<BaseFieldElement>(table_prover_factory);
+}
+
+void *run_bench(void *input, void *prover_factory, size_t trace_length, size_t blowup) {
+    // Random trace
+    auto trace_columns = static_cast<std::vector<std::vector<BaseFieldElement>> *>(input);
+
     // Config
     bool eval_in_natural_order = true;
     size_t n_columns = 1;
 
-    // Prover setup
-    Prng prng;
-    ProverChannel prover_channel(ProverChannel(prng.Clone()));
-
-    TableProverFactory<BaseFieldElement> table_prover_factory =
-        GetTableProverFactory<BaseFieldElement>(&prover_channel);
-
-    EvaluationDomain *evaluation_domain = new EvaluationDomain(trace_length, blowup);
-    auto prover = new CommittedTraceProver<BaseFieldElement>(UseOwned(evaluation_domain), n_columns, table_prover_factory);
-      //std::cout << "Domain ugh Length " << evaluation_domain.TraceSize() << "\n";
-
-
-    return prover;
-}
-
-
-void run_bench(void *input, void *prover, size_t trace_length, size_t blowup) {
-    // Config
-    bool eval_in_natural_order = true;
-
-    // Random trace
-    auto trace_columns = static_cast<std::vector<std::vector<BaseFieldElement>> *>(input);
-    std::vector<std::vector<BaseFieldElement>> trace(trace_columns->begin(), trace_columns->end());
-
-    // Prover
-    auto trace_prover = static_cast<CommittedTraceProver<BaseFieldElement> *>(prover);
-    EvaluationDomain evaluation_domain(trace_length, blowup);
-    auto domain = evaluation_domain.TraceDomain();
-    //std::cout << "Domain wtf Length " << domain.TraceSize() << "\n";
+    TableProverFactory<BaseFieldElement> *table_prover_factory = static_cast<TableProverFactory<BaseFieldElement> *>(prover_factory);
 
     // Commit
-    trace_prover->Commit(
-        TraceBase<BaseFieldElement>(std::move(trace)),
-        evaluation_domain.TraceDomain(),
-        eval_in_natural_order
-    );
+    const EvaluationDomain evaluation_domain(trace_length, blowup);
+    CommittedTraceProver<BaseFieldElement> prover(UseOwned(&evaluation_domain), n_columns, *table_prover_factory);
+    std::vector<std::vector<BaseFieldElement>> trace(trace_columns->begin(), trace_columns->end());
+
+    prover.Commit(
+        TraceBase<BaseFieldElement>(std::move(trace)), evaluation_domain.TraceDomain(),
+        eval_in_natural_order);
 }
 }
